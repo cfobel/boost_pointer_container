d810e1e7df7e4162a25406ff0b37f8c6d5ac96a0
Christian Fobel <christian@fobel.net>
2010-10-04 19:47:54 -0400

    Add modification of ptr_vector values in-place.

--- ptr_vector.cpp
+++ ptr_vector.cpp
@@ -7,19 +7,27 @@ using namespace std;
 
 int main() 
 { 
-    boost::ptr_vector<int> v; 
+    typedef boost::ptr_vector<int> ptr_vec;
+    ptr_vec v; 
+
     v.push_back(new int(1)); 
     v.push_back(new int(2)); 
 
-    std::auto_ptr<int> i;
+    std::auto_ptr<int> w;
 
-    BOOST_FOREACH(int w, xrange(1, 11)) {
-        i = std::auto_ptr<int>(new int(10 * w));
-        v.push_back(i);
+    BOOST_FOREACH(int i, xrange(1, 11)) {
+        w = std::auto_ptr<int>(new int(10 * i));
+        v.push_back(w);
     }
 
-    BOOST_FOREACH(int w, v) {
-        cout << w << endl;
+    /* Modify values in ptr_vec in-place */
+    BOOST_FOREACH(int &i, v) {
+        cout << i << endl;
+        i *= 2;
+    }
+
+    BOOST_FOREACH(int i, xrange(v.size())) {
+        cout << "[" << i << "]: " << v[i] << endl;
     }
 
     return 0;
